bf09fa912e32b07cd58b7376db89c1df410cdacc
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2025-08-26 19:54:56 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const API_KEY = "AIzaSyA6crBKIIcjw6WbG-jaobiswZXnpxYJ0T4";
+export const API_KEY = "AIzaSyAqN_GMztmKGXR5LO9SVA-gvK5yuciw9K8";
 export const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 export const SUBJECTS = {
